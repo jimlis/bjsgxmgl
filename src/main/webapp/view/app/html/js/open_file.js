259842b83905c8883face0cc4650d27c8cff164a
704966944@qq.com <lfx801271>
2018-09-24 12:10:15 +0800

    app，修改文档在线打开

--- src/main/webapp/view/app/html/js/open_file.js
+++ src/main/webapp/view/app/html/js/open_file.js
@@ -1,12 +1,18 @@
 
-
-
+function reinitIframe(){
+	var iframe = document.getElementById("fileIframe");
+	try{
+		var bHeight = window.screen.height;
+		//var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+		iframe.height = bHeight;
+		console.log(bHeight);
+	}catch (ex){
+		
+	}
+}
+reinitIframe();
 var id=getQueryString("id");
-
-
 openFile(id);
-
-
 function openFile(id) {
-	document.getElementById("fileIframe").setAttribute("src",serverPath+"/js/plugins/PDFViewer/web/viewer.html?file=/api/file/down/"+id);
+	document.getElementById("fileIframe").setAttribute("src",serverPath+"plugins/PDFViewer/web/viewer.html?file="+serverPath+"api/file/down/"+id);
 }
