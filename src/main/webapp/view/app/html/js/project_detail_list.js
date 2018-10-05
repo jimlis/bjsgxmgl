c3d4134094df3d706a21a1fdc8e629ddfdfeab78
704966944@qq.com <lfx801271>
2018-10-05 15:38:47 +0800

    新增项目列表页面

--- src/main/webapp/view/app/html/js/project_detail_list.js
+++ src/main/webapp/view/app/html/js/project_detail_list.js
@@ -17,6 +17,30 @@ function openNext(id){
 		case 3:
 		address="project_photo_record_list.html"
 			break;
+		case 4:
+		address="project_progress_record.html"
+			break;
+		case 5:
+		address="project_templet_record.html"
+			break;
+		case 6:
+		address="project_quality_report.html"
+			break;
+		case 7:
+		address="project_material_record.html"
+			break;
+		case 8:
+		address="project_change_record.html"
+			break;
+		case 9:
+		address="project_pay_record.html"
+			break;
+		case 10:
+		address="project_safe_report.html"
+			break;
+		case 11:
+		address="project_gov_record.html"
+			break;
 		default:
 			break;
 	}
