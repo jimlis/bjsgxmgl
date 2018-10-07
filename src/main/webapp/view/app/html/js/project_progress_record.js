d5dbe748302efe0bc7b72acdac6b8e7757e74a1e
704966944@qq.com <lfx801271>
2018-10-07 12:27:49 +0800

    施工进度html

--- src/main/webapp/view/app/html/js/project_progress_record.js
+++ src/main/webapp/view/app/html/js/project_progress_record.js
@@ -1,4 +1,27 @@
 
+
+document.getElementById("confirmBtn").addEventListener('tap', function() {
+	var btnArray = ['基础施工', '否', '否', '否', '否'];
+	mui.confirm('MUI是个好框架，确认？', 'Hello MUI', btnArray, function(e) {
+		switch (e.index){
+			case 0:
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default:
+				break;
+		}
+		bjToast("提升错误信息");
+	})
+});
 function openNextAdd(id){
 		toUrl("project_photo_record_add.html?id="+id);
 }
