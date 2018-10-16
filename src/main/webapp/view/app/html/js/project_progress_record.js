5f6707c351a262902d825d53740a2ff1ce2a54bc
704966944@qq.com <lfx801271>
2018-10-16 22:36:47 +0800

    施工进度基础施工

--- src/main/webapp/view/app/html/js/project_progress_record.js
+++ src/main/webapp/view/app/html/js/project_progress_record.js
@@ -5,8 +5,44 @@ window.onload=function(){
 	//得到数据
 	var PageData = getPageData();
 	//绑定数据
+	var vue = new Vue({
+		el: '#app',
+		data: {jcs:pageData.jc,
+		zts:pageData.zt,
+		ers:pageData.ec,
+		dts:pageData.dt,
+		sws:pageData.sw,
+		yls:pageData.yl},
+		methods: {
+			openJC: function (id) {
+				var address = "project_progress_record_baseDetail.html?id="+id;
+    				toUrl(address);
+			},
+			openZT: function (id) {
+				var address = "project_progress_record_BodyDetail.html?id="+id;
+    				toUrl(address);
+			},
+			openEC: function (id) {
+				var address = "project_progress_record_SecDetail.html?id="+id;
+    				toUrl(address);
+			},
+			openDT: function (id) {
+				var address = "project_progress_record_ElevatorDetail.html?id="+id;
+    				toUrl(address);
+			},
+			openSW: function (id) {
+				var address = "project_progress_record_outdoorDetail.html?id="+id;
+    				toUrl(address);
+			},
+			openYL: function (id) {
+				var address = "project_progress_record_gardenDetail.html?id="+id;
+    				toUrl(address);
+			},
+		}
+	});
 	
 }
+//初始化新增跳转页面
 function init(){
 	var btnArray = [{
 			value: '0',
@@ -27,58 +63,30 @@ function init(){
 			value: '5',
 			text: '园林施工'
 	}];
-	document.getElementById("confirmBtn").onclick = function(){
-		vuePicker('confirmBtn',btnArray,null,function(obj){
-			switch (obj.value){
-				case "0":
-				toUrl("project_progress_record_baseAdd.html");
-					break;
-				case "1":
-				toUrl("project_progress_record_bodyAdd.html");
-					break;
-				case "2":
-				toUrl("project_progress_record_SecDetail.html");
-					break;
-				case "3":
-				toUrl("project_progress_record_elevatorAdd.html");
-					break;
-				case "4":
-				toUrl("project_progress_record_outdoorAdd.html");
-					break;
-				case "5":
-				toUrl("project_progress_record_gardenAdd.html");
-					break;
-				default:
-					break;
-			}
-		});
-	}
-	
-	
-}
-function openNext(type,id){
-	switch (type){
+	relPicker('confirmBtn',btnArray,null,function(obj){
+		switch (obj.value){
 			case "0":
-			toUrl("project_progress_record_baseDetail.html?id="+id);
+			toUrl("project_progress_record_baseAdd.html");
 				break;
 			case "1":
-			toUrl("project_progress_record_BodyDetail.html?id="+id);
+			toUrl("project_progress_record_bodyAdd.html");
 				break;
 			case "2":
-			toUrl("project_progress_record_SecDetail.html?id="+id);
+			toUrl("project_progress_record_SecDetail.html");
 				break;
 			case "3":
-			toUrl("project_progress_record_ElevatorDetail.html?id="+id);
+			toUrl("project_progress_record_elevatorAdd.html");
 				break;
 			case "4":
-			toUrl("project_progress_record_outdoorDetail.html?id="+id);
+			toUrl("project_progress_record_outdoorAdd.html");
 				break;
 			case "5":
-			toUrl("project_progress_record_gardenDetail.html?id="+id);
+			toUrl("project_progress_record_gardenAdd.html");
 				break;
 			default:
 				break;
 		}
+	});
 }
 //得到基础、主体、二次、电梯、室外、园林信息数据
 function getPageData(){
@@ -89,6 +97,6 @@ function getPageData(){
 	    	},
 	    	success:function (data) {
 	    		return data;
-	    }
+	        }
     });
 }
--- src/main/webapp/view/app/html/js/project_progress_record_baseAdd.js
+++ src/main/webapp/view/app/html/js/project_progress_record_baseAdd.js
@@ -1,25 +1,109 @@
-
+var id = getRequest().id;
+var pageData;
+var vue;
 window.onload = function(){
-	dtPicker('#showDateJZS');
-	upLoadImg('#chbtn','#upbtn');
-	relPicker("showAddress",getPageData("sgwz"));
-	relPicker("showType",getPageData("pslx"));
-	
-}
-function getPageData(type){
-	var data;
-	switch (type){
-		case "sgwz":
-		data = [{text:"#1栋",value:"1"},{text:"其他",value:"2"}]
-			break;
-		case "pslx":
-		data = [{text:"独立基础",value:"1"},{text:"筏板",value:"2"}]
-			break;
-		default:
-			break;
+	debugger
+	//初始化數據
+	var sgPickerData  ;
+	var jcPickerData = getJC();
+	//判断是否更新；
+	pageData = isUpdata()||'';
+	if(pageData==''){
+		//创建数据Model；
+		pageData = buildModel();
 	}
-	return data;
+	//数据绑定
+	vue = new Vue({
+		el: '#app',
+		data: pageData,
+		methods: {
+			sgPicker: function (event) {
+				vuePicker(pageData,"chrxclb",sgPickerData,"intxclb");
+			},
+			jcPicker: function (event) {
+				vuePicker(pageData,"chrxcbm",jcPickerData,"intxcbm");
+			},
+			datePicker: function (event) {
+				vueDtPicker(pageData,"dtmxcrq");
+			},
+		}
+	});
 }
+//判断是否更新
+function isUpdata(){
+	var result={};
+	if(id){
+		$bjAjax({
+			url:progressJCApiGet,
+			type:"post",
+			async:false,
+			data:{
+				xmSgjdJcsgId:obj.id
+			},
+			success:function(data){
+				result = data;
+			}
+		});
+	}
+	return result;
+}
+//创建数据Model
+function buildModel(){
+	var model = {
+		id:'1',
+		intxmid:'3',
+		dtmgxrq:'2018-9-8',
+		intxclb:'',
+		chrxclb:'',
+		intxcbm:'',
+		chrxcbm:'',
+		chrxcry:'',
+		dtmxcrq:'',
+		chrzb:'',
+		intbgrid:'52',
+		chrbgrmc:'测试'
+	}
+	return model;
+}
+//初始化下拉框【施工位置】数据
+function getSG(){
+	var result=new Array();
+	$bjAjax({
+			url:----,
+			type:"post",
+			async:false,
+			data:{
+				xmSgjdJcsgId:obj.id
+			},
+			success:function(data){
+				result = data;
+			}
+	});
+	return result;
+}
+//初始化下拉框【基础类型】数据
+function getJC(){
+	var result=[{"text":"独立基础","value":"1"},{"text":"筏板","value":"2"},{"text":"桩基础","value":"3"}];
+	debugger
+	return result;
+}
+
+//保存数据
 function save(){
-	console.log("baoc");
+	
+	var data = getFromData("myform");
+	bjConsole(data);
+	if(obj.id){
+		data["id"] = obj.id;
+	}
+	$bjAjax({
+		url:xmzfxcyzxysApiSave,
+		data:data,
+		type:"post",
+		success:function(data){
+			bjToast("保存成功！",function(){
+				toUrl("project_gov_record_details.html?id="+data.id);
+			});
+		}
+	});
 }
