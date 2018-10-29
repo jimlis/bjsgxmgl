4a1b2db720d2589adbbea15c68e31c3e02f79a6f
704966944@qq.com <lfx801271>
2018-10-30 00:38:56 +0800

    钉钉发送消息

---
+++ src/main/java/com/zj/platform/common/dingding/sendMessage.java
@@ -0,0 +1,51 @@
+package com.zj.platform.common.dingding;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.zj.platform.common.util.CommonUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jimli on 2018/10/29.
+ */
+public class sendMessage {
+    //得到token
+    private static String getToken(String getTokenPath) throws Exception{
+        String tokenResult= CommonUtils.HttpURLConnectionGet(getTokenPath);
+        java.lang.reflect.Type type = new TypeToken<HashMap<String, String>>() {}.getType();
+        Map<String ,String> tokenMap = new Gson().fromJson(tokenResult, type);
+        return tokenMap.get("access_token");
+    }
+    /*
+        contentTest:发送内容
+        agentid：应用id
+        touser：接收人
+     */
+    public static String sendMessage(String corpid,String corpsecret,String contentTest,String agentid,String touser) throws Exception{
+        String getTokenPath = "https://oapi.dingtalk.com/gettoken?corpid="+corpid+"&corpsecret="+corpsecret;
+        String access_token = sendMessage.getToken(getTokenPath);
+
+        JSONObject image=new JSONObject();
+        JSONObject text=new JSONObject();
+        text.put("content",contentTest);
+        image.put("text", text);
+        image.put("msgtype","text");
+        image.put("agentid",agentid);
+        image.put("touser",touser);
+        System.out.println(image.toString());
+        JSONObject result=null;
+        String url = "https://oapi.dingtalk.com/message/send?access_token="+access_token;
+        try{
+            result=CommonUtils.httpPost(url, image);
+        }catch(Exception e) {
+        //TODOAuto-generated catch block
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+
+}
--- src/main/java/com/zj/platform/common/util/CommonUtils.java
+++ src/main/java/com/zj/platform/common/util/CommonUtils.java
@@ -1,9 +1,21 @@
 package com.zj.platform.common.util;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.dingtalk.oapi.lib.aes.DingTalkJsApiSingnature;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.util.EntityUtils;
+import org.springframework.http.HttpEntity;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -41,5 +53,57 @@ public class CommonUtils {
             e.getMessage();
         }
         return "";
+    }
+    //post请求
+    public static JSONObject httpPost(String url, Object data) throws Exception {
+        HttpPost httpPost = new HttpPost(url);
+        CloseableHttpResponse response = null;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        RequestConfig requestConfig = RequestConfig.custom().
+                setSocketTimeout(3000).setConnectTimeout(3000).build();
+        httpPost.setConfig(requestConfig);
+        httpPost.addHeader("Content-Type", "application/json");
+
+        try {
+            StringEntity requestEntity = new StringEntity(JSON.toJSONString(data), "utf-8");
+            httpPost.setEntity(requestEntity);
+
+            response = httpClient.execute(httpPost, new BasicHttpContext());
+
+            if (response.getStatusLine().getStatusCode() != 200) {
+
+                System.out.println("request url failed, http code=" + response.getStatusLine().getStatusCode()
+                        + ", url=" + url);
+                return null;
+            }
+            org.apache.http.HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                String resultStr = EntityUtils.toString(entity, "utf-8");
+
+                JSONObject result = JSON.parseObject(resultStr);
+                if (result.getInteger("errcode") == 0) {
+                    result.remove("errcode");
+                    result.remove("errmsg");
+                    return result;
+                } else {
+                    System.out.println("request url=" + url + ",return value=");
+                    System.out.println(resultStr);
+                    int errCode = result.getInteger("errcode");
+                    String errMsg = result.getString("errmsg");
+                    throw new Exception();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("request url=" + url + ", exception, msg=" + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (response != null) try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
 }
