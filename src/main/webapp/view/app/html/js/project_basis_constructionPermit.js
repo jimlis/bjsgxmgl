e9256515622452ca429bd3470c00fb49ca9b4362
李飞翔 <704966944@qq.com>
2019-01-18 14:29:15 +0800

    修改2019-1-18调整样式

--- src/main/webapp/view/app/html/js/project_basis_constructionPermit.js
+++ src/main/webapp/view/app/html/js/project_basis_constructionPermit.js
@@ -10,8 +10,8 @@ $bjAjax({
 		var id=data.id||"";
 		document.getElementById("chrxkzbh").innerText = data.chrxkzbh||"";
 		document.getElementById("chrffbm").innerText = data.chrffbm||"";
-	//	document.getElementById("dtmfzrq").innerText = data.dtmfzrq||"";
-	//document.getElementById("dtmyxrq").innerText = data.dtmyxrq||"";
+		document.getElementById("dtmfzrq").innerText = data.dtmfzrq||"";
+		document.getElementById("dtmyxrq").innerText = data.dtmyxrq||"";
 		initFileList("bj_xm_xkz",id,"1","fileIds","file-list",false);
 	}
 })
